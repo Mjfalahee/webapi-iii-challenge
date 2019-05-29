552bdd4a7a2d6ab12cc5a42ea609add6b8fe92e8
mjfalahee <mjfalahee@gmail.com>
2019-05-29 14:42:59 -0700

    Logger done

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
+
 
 const server = express();
+
+server.use(
+  express.json(),
+  helmet(), 
+  logger
+  );
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+
+
 //custom middleware
 
-function logger(req, res, next) {
 
+function logger(req, res, next) {
+const currentTime = new Date();
+const clockTime = "At: " + currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+
+console.log(`A ${req.method} request to '${req.url}'`);
+console.log(`${clockTime}`)
+next();
 };
 
 module.exports = server;
